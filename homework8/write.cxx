55f6a3f80a812cf1b1ecf083140555becfc375d0
Adib Shaker <neoadib@gmail.com>
2024-10-29 17:49:43 +0100

    added write.cxx

---
+++ homework8/write.cxx
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <TRandom.h>
+#include <TTree.h>
+#include <TFile.h>
+#include <TF1.h>
+#include "momentum.h"
+
+void writeMomentum() {
+    
+    // Number of events
+    const Int_t nEvent = 1000;
+
+    // Initialize your object e.g. as a pointer
+    momentum *event;
+
+    // Create your root file here    
+    TFile f("tree_file.root", "RECREATE");
+
+    // Create your TTree here
+    TTree *tree = new TTree("tree", "momentum tree");
+
+    // And the associated branches underneath
+    tree->Branch("event", &event);
+
+    // Now we create our loop for filling the tree with some random data
+    Double_t px, py, pz;
+    Int_t eventID;
+
+    // For loop here
+    for (Int_t i{0}; i < nEvent; i++) {
+        // Initialize your new object below
+        eventID = i;
+        px = gRandom->Gaus(0, 0.02);
+        py = gRandom->Gaus(0, 0.02);
+        pz = gRandom->Gaus(0, 0.02);
+        // Now fill tree
+        event = new momentum(eventID, px, py, pz);
+        tree->Fill();
+        // Remember to delete it again otherise you will have memory leak!
+        delete event;
+    }
+    // save the tree/file
+    tree->Write();
+    tree->AutoSave();
+}
