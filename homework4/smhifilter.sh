41a1e49a496d7afcf79e93646b830746e3ccaef8
Adib Shaker <neoadib@gmail.com>
2024-11-05 18:35:56 +0100

    remove sorted filter

--- homework4/smhifilter.sh
+++ homework4/smhifilter.sh
@@ -160,11 +160,6 @@ FILTER_FILTERFILENAME_HEAD="head__$FILTER_FILTEREDFILENAME"
 log "Filtering on only the head of the filename"
 head -10 $CLEANER_BAREDATAFILENAME > output/$FILTER_FILTERFILENAME_HEAD
 
-# Show only the sorted tail of the filename
-FILTER_FILTERFILENAME_SORTED_TAIL="sortedTail__$FILTER_FILTEREDFILENAME"
-log "Filtering on only the sorted tail of the filename"
-sort $CLEANER_BAREDATAFILENAME | tail -10 > output/$FILTER_FILTERFILENAME_SORTED_TAIL
-
 # Select the measurements with temperature between 5 to 10 degrees
 FILTER_FILTERFILENAME_TEMP_RANGE="temp_range_$FILTER_FILTEREDFILENAME"
 log "Filtering on temperatures between 5 and 10 degrees, writing to $FILTER_FILTERFILENAME_TEMP_RANGE"
